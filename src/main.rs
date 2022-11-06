e8417ef6d31a984273fe8b41b5cfdd633c6a45c4
Sangram Reddy <reddy.horcrux@gmail.com>
2022-11-06 23:03:22 +0000

    Correcting wheel rotations

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,19 @@ use std::error::Error;
 use std::thread;
 use std::time::Duration;
 
-const GPIO_21: u8 = 21; // FL: In1
-const GPIO_20: u8 = 20; // FL: In2
-const GPIO_16: u8 = 16; // FR: In3
-const GPIO_12: u8 = 12; // FR: In4
+const GPIO_21: u8 = 21;
+const GPIO_20: u8 = 20;
+const GPIO_16: u8 = 16;
+const GPIO_12: u8 = 12;
 
-const GPIO_19: u8 = 19; // RL: In3
-const GPIO_26: u8 = 26; // RL: In4
-const GPIO_23: u8 = 23; // RR: In1
-const GPIO_24: u8 = 24; // RR: In2
+const GPIO_19: u8 = 19;
+const GPIO_26: u8 = 26;
+const GPIO_23: u8 = 23;
+const GPIO_24: u8 = 24;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let engine = Engine::new_from_gpio_pins(
-        GPIO_21, GPIO_20, GPIO_16, GPIO_12, GPIO_19, GPIO_26, GPIO_23, GPIO_24,
+        GPIO_24, GPIO_23, GPIO_26, GPIO_19, GPIO_12, GPIO_16, GPIO_20, GPIO_21,
     )?;
 
     let mut aston_martin: Car = Car::new(engine)?;
