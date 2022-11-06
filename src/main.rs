6171a7aa9a24fe06a5fbdf166da33e2fd435d643
Sangram Reddy <reddy.horcrux@gmail.com>
2022-11-06 21:15:29 +0000

    Initial version of the `robo` package

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ const GPIO_20: u8 = 20; // FL: In2
 const GPIO_16: u8 = 16; // FR: In3
 const GPIO_12: u8 = 12; // FR: In4
 
-const GPIO_06: u8 = 6; // BR: In1
-const GPIO_13: u8 = 13; // BR: In2
-const GPIO_19: u8 = 19; // BL: In3
-const GPIO_26: u8 = 26; // BL: In4
+const GPIO_19: u8 = 19; // RL: In3
+const GPIO_26: u8 = 26; // RL: In4
+const GPIO_23: u8 = 23; // RR: In1
+const GPIO_24: u8 = 24; // RR: In2
 
 fn main() -> Result<(), Box<dyn Error>> {
     let engine = Engine::new_from_gpio_pins(
-        GPIO_21, GPIO_20, GPIO_16, GPIO_12, GPIO_19, GPIO_26, GPIO_06, GPIO_13,
+        GPIO_21, GPIO_20, GPIO_16, GPIO_12,GPIO_19, GPIO_26,GPIO_23, GPIO_24
     )?;
 
     let mut aston_martin: Car = Car::new(engine)?;
@@ -26,15 +26,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     loop {
         println!("[INFO] Stopping");
         aston_martin.stop();
-        thread::sleep(Duration::from_millis(5000));
+        thread::sleep(Duration::from_millis(10000));
         println!("[INFO] Driving");
         aston_martin.drive();
-        thread::sleep(Duration::from_millis(10000));
-        println!("[INFO] Stoping");
+        thread::sleep(Duration::from_millis(3000));
+        println!("[INFO] Stopping");
         aston_martin.stop();
-        thread::sleep(Duration::from_millis(5000));
+        thread::sleep(Duration::from_millis(10000));
         println!("[INFO] Reversing");
         aston_martin.reverse();
-        thread::sleep(Duration::from_millis(10000));
+        thread::sleep(Duration::from_millis(3000));
     }
 }
